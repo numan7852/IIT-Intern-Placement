2e7adad2b2e9cfc31d6d65eb1da43b0c68e9879a
Unknown <bsse0624@iit.du.ac.bd>
2017-11-22 01:47:38 +0600

    Welcome Page Title Change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    IIT IPOS
                 </div>
 
                 <div class="links">
